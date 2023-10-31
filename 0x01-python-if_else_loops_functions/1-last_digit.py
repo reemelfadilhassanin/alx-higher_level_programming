1d59ea3609d1db63f62c49d7f71c6ebd69e6dfe9
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-10-31 23:50:59 +0400

    commit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+md = abs(number) % 10
+if number < 0:
+   md = -md
+print("Last digit of {} is {} and is ".format(number, md), end="")
+if md > 5:
+    print("greater than 5")
+elif md == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")
