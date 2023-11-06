321d002f4652f1c2d0f72d118669ef46c1ccf811
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-06 11:36:11 +0400

    delete

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if int(idx) < 0 or int(idx) + 1 > len(my_list):
+    if idx < 0 or int(idx) + 1 > len(my_list):
         return my_list
     else:
         del my_list[idx]
