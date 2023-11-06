ab096edb7eb79a4a5e44eb92f9fae92f473b5be2
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-06 10:36:53 +0400

    7-main

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,9 +2,14 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     list_a = list(tuple_a)
     list_b = list(tuple_b)
-    while len(list_a) < 2:
-        list_a.append(0)
-    while len(list_b) < 2:
-        list_b.append(0)
-    res = [list_a[0] + list_b[0], list_a[1] + list_b[1]]
-    return(tuple(res))
+
+    if len(list_a) < 2:
+        for i in range(len(list_a), 2):
+            list_a.append(0)
+
+    if len(list_b) < 2:
+        for i in range(len(list_b), 2):
+            list_b.append(0)
+
+    list_c = [list_a[0] + list_b[0], list_a[1] + list_b[1]]
+    return (tuple(list_c))
