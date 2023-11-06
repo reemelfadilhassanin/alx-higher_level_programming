f95eea152b3a0de2705ebf9d6ab384be7c542cb2
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-06 09:12:14 +0400

    fix

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for c in my_list:
-        print("{}".format(c))
+    for i in range(len(my_list)):
+        print("{}".format(my_list[i]))
