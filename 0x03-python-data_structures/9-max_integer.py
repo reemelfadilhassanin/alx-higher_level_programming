7eed62b8675e218be11059a503a38fed109a7d22
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-06 11:14:43 +0400

    9-main

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not len(my_list):
-         return None
+    if not my_list:
+        return None
     else:
         max = my_list[0]
-        for i in my_list[1:]:
-            max = max if i <= max else i
+        for i in my_list:
+            if i > max:
+                max = i
+           
         return max
