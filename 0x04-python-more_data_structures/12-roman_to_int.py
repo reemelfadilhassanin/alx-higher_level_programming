34c7773fe67055e6da9c490794ffe161255a04a8
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-08 22:07:16 +0400

    12-main.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str):
-        return 0
-    total = 0
-    num = 0
-    digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 
-    'C': 100, 'D': 500, 'M': 1000}
-    for i in reversed(roman_string):
-        num = digits[i]
-        total += num if total < num * 5 else -num
-    return total
+        roman_dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                            'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+        item = 0
+        roman = roman_string
+        if type(roman) is not str or len(roman) is 0:
+                return 0
+        for item in range(item, len(roman)):
+                if item < len(roman) - 1\
+                   and\
+                   roman_dictionary[roman[item]] <\
+                   roman_dictionary[roman[item + 1]]:
+                    result -= roman_dictionary[roman[item]]
+                else:
+                    result += roman_dictionary[roman[item]]
+        return result
