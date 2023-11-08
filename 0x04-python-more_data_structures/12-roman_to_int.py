f2f03bdf68f0c371458aa2dd85fefe27af9d5d0f
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-08 22:42:40 +0400

    12-main.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,7 @@
 def roman_to_int(roman_string):
     if not roman_string:
         return 0
-    if not isinstance(roman_string, str):
-        return 0
+   
 
     roman_dec = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                     'C': 100, 'D': 500, 'M': 1000}
