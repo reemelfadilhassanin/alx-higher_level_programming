24c8178733ffa62a8cf082d50ee8d1915baae2d8
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-08 22:38:34 +0400

    12-main.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if not roman_string:
+    if not roman_string or not isinstance(roman_string, str):
         return 0
-    if not isinstance(roman_string, str):
-        return 0
+
+    roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                    'C': 100, 'D': 500, 'M': 1000}
+
     result = 0
-    roman_dec = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     i = 0
     while i < len(roman_string):
-        str1 = roman_dec(roman_string[i])
+        current_value = roman_values.get(roman_string[i], 0)
+
         if i + 1 < len(roman_string):
-            str2 = roman_dec(roman_string[i + 1])
-            if str1 >= str2:
-                result = result + str1
-                i = i + 1
+            next_value = roman_values.get(roman_string[i + 1], 0)
+            if current_value >= next_value:
+                result += current_value
+                i += 1
             else:
-                result = result + str2 - str1
-                i = i + 2
+                result += next_value - current_value
+                i += 2
         else:
-            result = result + str1
-            i = i + 1
+            result += current_value
+            i += 1
+
     return result
