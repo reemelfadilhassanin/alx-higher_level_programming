0c9be1cf63294747451375e424d70c625f4ad892
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-08 21:57:31 +0400

    0-main.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
- return [[element ** 2 for element in row] for row in matrix]
+    result = []
+    for i in range(len(matrix)):
+        result += [list(map(lambda x: x * x, matrix[i]))]
+    return result
