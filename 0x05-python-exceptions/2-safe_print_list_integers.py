602ca98978525e8d6ce369d791467288a93d68f8
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-20 20:46:38 +0400

    printfirstint

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-	index = 0
-	for i in range(0, x):
-		x = my_list[i]
-		try:
-			print("{:d}".format(x), end='')
-			index = index + 1
-		except (ValueError, TypeError):
-			pass
-		print()
-		return index
+    index = 0
+    for i in range(x):
+        try:
+            x = my_list[i]
+            if type(x) == int:
+                print("{:d}".format(x), end='')
+                index = index + 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    return index
