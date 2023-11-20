efad3e8db284608ca875c81cfc9bf7d629304c3d
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-20 21:46:04 +0400

    magic function

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,9 +3,9 @@ import sys
 
 
 def safe_print_integer_err(value):
-    try:
-        print("{:d}".format(value))
-        return True
-    except (ValueError, TypeError) as e:
-        print("Exception:", e, file=sys.stderr)
-        return False
+	try:
+		print("{:d}".format(value))
+		return True
+	except (ValueError, TypeError) as e:
+		print("Exception:", e, file=sys.stderr)
+		return False
--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    result = 0
-    for i in range(1, 3):
+    count = 0
+    for x in range(1, 3):
         try:
-            if i > a:
+            if x > a:
                 raise Exception("Too far")
-            result += (a ** b) / i
+            count += (a ** b) / x
         except Exception:
-            result = b + a
+            count = b + a
             break
-    return result
+    return count
