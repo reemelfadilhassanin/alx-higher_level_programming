56a90663667cb69103f82a1351b335f94a2a900a
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-20 19:55:59 +0400

    typeerror&valueerror

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,6 @@
 def safe_print_integer(value):
 	try:
 		print("{:d}".format(value))
-	except (TypeError, ValueError):
+		return (True)
+	except Exception as e:
 		return (False)
-	else:
-			return (True)
