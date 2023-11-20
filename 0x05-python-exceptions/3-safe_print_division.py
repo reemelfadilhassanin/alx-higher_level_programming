064b4649d07a64ffa8ff14b69b45bc9f1b480f46
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-20 20:53:20 +0400

    zerodivisionerror

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        result = a / b
-    except ZeroDivisionError:
-        result = None
-        print("Inside result: {}".format(result))
-        return result
+        res = a / b
+    except Exception as e:
+        res = None
+    finally:
+        print("Inside result: {}".format(res))
+        return res
