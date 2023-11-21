d8c164d2325843a413d07864f07470b9bb1fd7db
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-21 21:57:00 +0400

    type size square class

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,10 +12,10 @@ class Square:
             size: private instance size of the square.
         """
         self.__size = size
-
         if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
+        
