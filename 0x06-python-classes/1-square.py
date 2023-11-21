bdebf40566f944fa14ad17b6804eb7f1fd3676ed
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-21 21:33:27 +0400

    private size square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This define square class"""
+
+
+class Square:
+    """Class has no attribute and construct have none.
+    """
+    def __init__(self, size):
+        """Constructor of square with one instance size.
+
+        Args:
+            size: private instance size of the square.
+        """
+        self.__size = size
