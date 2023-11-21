f000d9a91ff9c3a5a90e2140fa03adb8c82e97b7
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-22 02:06:03 +0400

    linklist

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -85,7 +85,7 @@ class SinglyLinkedList:
         """
         temp = self.__head
         print_node = []
-        while temp is not None and temp.next_node.data < value:
+        while temp is not None:
             print_node.sort()
             print_node.append(str(temp.data))
             temp = temp.next_node
