63044525b7cd1772096deda679d02905394fbf93
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-22 01:57:14 +0400

    linklist

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -3,108 +3,108 @@
 
 
 class Node:
-	"""
-	Class Node.
+    """
+    Class Node.
 
-	Attributes:
-		data: data of node.
-	"""
-	def __init__(self, data, next_node=None):
-		"""Creates constructor of node.
+    Attributes:
+        data: data of node.
+    """
+    def __init__(self, data, next_node=None):
+        """Creates constructor of node.
 
-		Args:
-			__data : data field of linklist.
-		"""
-		self.data = data
-		self.next_node = next_node
+        Args:
+            __data : data field of linklist.
+        """
+        self.data = data
+        self.next_node = next_node
 
-	@property
-	def data(self):
-		"""Getter method.
+    @property
+    def data(self):
+        """Getter method.
 
-		Returns: int value of data field.
-		"""
-		return self.__data
+        Returns: int value of data field.
+        """
+        return self.__data
 
-	@data.setter
-	def data(self, value):
-		"""Method of setter for data.
+    @data.setter
+    def data(self, value):
+        """Method of setter for data.
 
-		Args:
-			value: int value for data field.
+        Args:
+            value (int): data field of a node.
 
-		Raises:
-			TypeError: if is not an integer
-		"""
-		if not isinstance(value, int):
-			raise TypeError("data must be an integer")
-		self.__data = value
+        Raises:
+            TypeError: data must be an integer
+        """
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
 
-	@property
-	def next_node(self):
-		"""Gette method for the next_node instance.
+    @property
+    def next_node(self):
+        """Retrives the next_node instance.
 
-		Returns: Value of next_node instance.
-		"""
-		return self.__next_node
+        Returns: The next_node instance.
+        """
+        return self.__next_node
 
-	@next_node.setter
-	def next_node(self, value):
-		"""Method setter for Node.
+    @next_node.setter
+    def next_node(self, value):
+        """Property setter for Node.
 
-		Args:
-			value (None): next node of a Node.
+        Args:
+            value (None): next node of a Node.
 
-		Raises:
-			TypeError: next_node must be a Node object .
-		"""
-		if value is not None and not isinstance(value, Node):
-			raise TypeError("next_node must be a Node object")
-		self.__next_node = value
+        Raises:
+            TypeError: next_node must be a Node object .
+        """
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
 
 
 class SinglyLinkedList:
-	"""
-	Class of SinglyLinkedList.
+    """
+    Class that defines properties of SinglyLinkedList.
 
-	Attributes:
-		head: head of the SinglyLinkedList.
-	"""
-	def __init__(self):
-		"""Construction of SinglyLinkedList .
+    Attributes:
+        head: head of the SinglyLinkedList.
+    """
+    def __init__(self):
+        """Creates new instances of SinglyLinkedList .
 
-		Args:
-			__head : head of the SinglyLinkedList .
-		"""
-		self.__head = None
+        Args:
+            __head : head of the SinglyLinkedList .
+        """
+        self.__head = None
 
-	def __str__(self):
-		"""Class objects as a string.
+    def __str__(self):
+        """Represents the class objects as a string.
 
-		Returns: The class object.
-		"""
-		temp_var = self.__head
-		print_node = []
-		while temp_var:
-			print_node.sort()
-			print_node.append(str(temp_var.data))
-			temp_var = temp_var.next_node
+        Returns: The class object represented as a string.
+        """
+        temp_var = self.__head
+        print_node = []
+        while temp_var:
+            print_node.sort()
+            print_node.append(str(temp_var.data))
+            temp_var = temp_var.next_node
 
-		print_node.sort(key=int)
-		return ("\n".join(print_node))
+        print_node.sort(key=int)
+        return ("\n".join(print_node))
 
-	def sorted_insert(self, value):
-		"""Inserts a new node .
+    def sorted_insert(self, value):
+        """Inserts a new node at a given position.
 
-		Args:
-			value: value.
-		"""
-		if self.__head is None:
-			new_node = Node(value)
-			new_node.next_node = self.__head
-			self.__head = new_node
-		else:
-			new_node = Node(value)
-			new_node.data = value
-			new_node.next_node = self.__head
-			self.__head = new_node
+        Args:
+            value: value.
+        """
+        if self.__head is None:
+            new_node = Node(value)
+            new_node.next_node = self.__head
+            self.__head = new_node
+        else:
+            new_node = Node(value)
+            new_node.data = value
+            new_node.next_node = self.__head
+            self.__head = new_node
