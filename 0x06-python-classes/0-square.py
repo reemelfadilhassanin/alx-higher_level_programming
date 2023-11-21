e8d1c6339d1da39a7a0581e0b6bc9a807eb6951d
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-21 21:11:13 +0400

    empty square class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """
+    define empty squear class
+    """
+
+    def __init__(self):
+        """
+        constructor have empty attribute for empty class
+        """
+        pass
