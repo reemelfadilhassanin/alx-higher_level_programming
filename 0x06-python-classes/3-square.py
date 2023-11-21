696ddf32fd357a6e875cf9266edc8dd410a3ec57
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-21 22:11:53 +0400

    cal area of square class

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This define square class"""
+
+
+class Square:
+	"""
+	Class has no attribute and construct have none.
+
+	Args:
+		size: private instance size of the square.
+	"""
+	def __init__(self, size=0):
+		"""Constructor of square with one instance size.
+
+		Args:
+		   size: private instance size of the square.
+		"""
+		self.__size = size
+
+		if not isinstance(size, int):
+			raise TypeError("size must be an integer")
+		elif size < 0:
+			raise ValueError("size must be >= 0")
+		else:
+			self.__size = size
+	
+	def area(self):
+		"""Public method of the area of square.
+
+		Returns: method return current square area.
+		"""
+		return self.__size ** 2
