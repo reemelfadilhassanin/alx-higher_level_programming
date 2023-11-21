2059b8bfb646bb5057ea162ca7d165933a531c78
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-21 22:05:01 +0400

    type size square class

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,23 +3,23 @@
 
 
 class Square:
-    """Class has no attribute and construct have none.
     """
-    def __init__(self, size = 0):
+    Class has no attribute and construct have none.
+
+    Args:
+        size: private instance size of the square.
+    """
+    def __init__(self, size=0):
         """Constructor of square with one instance size.
 
         Args:
-            size: private instance size of the square.
-
-        Raises:
-        TypeError: If size is not an integer.
-        ValueError: If size is > 0.
+           size: private instance size of the square.
         """
         self.__size = size
-        
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
