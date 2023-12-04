4752a75559eb65cb325314d6b65bfea4bce52860
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-12-04 23:37:53 +0400

    type

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Define is_kind_of_class module
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """returns True if the object is an instance of,otherwise False"""
+    return (isinstance(obj, a_class))
