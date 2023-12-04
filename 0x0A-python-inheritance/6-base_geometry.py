fdbbb8ddd8c25a8feeb09923b7e3e534384e1861
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-12-04 23:50:27 +0400

    Public instance method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Define module BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Dfine Public instance method area"""
+
+    def area(self):
+        """raises exception if not implemented"""
+        raise Exception("area() is not implemented")
