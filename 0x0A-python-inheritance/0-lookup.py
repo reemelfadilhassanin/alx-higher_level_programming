95dc5a5e014609da2ff9e8db60f547aa32a5f39b
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-12-04 22:19:09 +0400

    dir

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+===========================================
+function that returns the list of attributes
+===========================================
+"""
+
+
+def lookup(obj):
+    """Function that returns the list of attributes and methods for an object"""
+
+    return dir(obj)
