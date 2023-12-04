42b3a1250ff0457d89f633872a3ea23a0e96ca10
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-12-05 00:07:41 +0400

    Full rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Define class Rectangle
+"""
+
+
+class BaseGeometry:
+    """Dfine Public instance method area"""
+
+    def area(self):
+        """raises exception if not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Dfine Public instance method integer_validator"""
+        if type(value) != int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Define class Rectangle"""
+
+    def __init__(self, width, height):
+        """define constructor"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return representation of the rectangle"""
+        return ("[Rectangle] " + str(self.__width) + "/" + str(self.__height))
