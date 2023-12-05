76b85185454320e729c87dbd8d7c2054c5baba48
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-12-05 22:20:24 +0400

    write using json object

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """ Define save_to_json_file module
 """
-
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """ function that writes an Object to a text file, using a JSON representation
+    """ Define function that writes an object to a text file
+    by a JSON representation
 
     Args:
-            my_obj (str): object (Python data structure) want to deserilize
+        my_obj: object
+        filename: the name of text file
+
     """
-    with open(filename, 'w') as f:
-        f.write(json.dumps(my_obj))
+    with open(filename, 'w', encoding="utf-8") as f:
+        json.dump(my_obj, f)
