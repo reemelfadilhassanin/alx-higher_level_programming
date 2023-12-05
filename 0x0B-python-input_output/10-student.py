935ffe63b1ce78c69a738dd5e03f1825e41bba96
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-12-05 22:56:11 +0400

    attrs

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Deine Student module
+"""
+
+
+class Student:
+    """ a class Student that defines a student """
+
+    def __init__(self, first_name, last_name, age):
+        """ constructor to initilize objects of student """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Puplic method that returns directory """
+        if (type(attrs) == list and
+                all(type(x) == str for x in attrs)):
+            return {s: getattr(self, s) for s in attrs if hasattr(self, s)}
+        return self.__dict__
