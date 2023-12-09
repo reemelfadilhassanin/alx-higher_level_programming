c323a248e5394781d1ea3bb6b14b9e7324dd6a92
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-12-09 22:23:56 +0400

    validate att

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -17,32 +17,44 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """Define width of the rectangle"""
+        """Deines width of rectangle"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Protect witdth of the rectangle"""
+        """Define private attr of rectangle"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
     def height(self):
-        """Define height of rectangle"""
+        """Defines height of rectangle"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Setting the height of the rectangle"""
+        """Define private attr of rectangle"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
     def x(self):
-        """The x coordinate of the rectangle"""
+        """Define private attr of rectangle"""
         return self.__x
 
     @x.setter
     def x(self, value):
         """Define private attr of rectangle"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -53,5 +65,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """Define private attr of rectangle"""
-
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
