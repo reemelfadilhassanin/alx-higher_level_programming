90fe2909333af6c029d4a193710a119ea2f0a8dc
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-12-10 08:28:32 +0400

    validate size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,17 +9,17 @@ class Square(Rectangle):
     """Defines square class inharit from rectangle
 
     Args:
-            Rectangle:supper class
+                    Rectangle:supper class
     """
 
     def __init__(self, size, x=0, y=0, id=None):
         """constructor for initialize object of class
 
         Args:
-                size (int): size of square
-                x (int): x coordinate of square. Defaults to 0.
-                y (int): y coordinate of square. Defaults to 0.
-                id (int): id of square. Defaults to None.
+                        size (int): size of square
+                        x (int): x coordinate of square. Defaults to 0.
+                        y (int): y coordinate of square. Defaults to 0.
+                        id (int): id of square. Defaults to None.
         """
         super().__init__(size, size, x, y, id)
 
@@ -27,6 +27,17 @@ class Square(Rectangle):
         """Define string module for square.
 
         Returns:
-                return: return string format for square
+                        return: return string format for square
         """
         return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x,  self.y, self.width)
+
+    @property
+    def size(self):
+        """Module for getting size of Square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Module for setting size of Square."""
+        self.width = value
+        self.height = value
