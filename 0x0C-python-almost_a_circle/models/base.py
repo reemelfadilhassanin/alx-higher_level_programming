ca9475ef91471614f3316a22165fae93f826bec8
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-12-10 09:12:50 +0400

    serilizationusing dumps

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Defines a module for base class"""
+import json
+from json import dumps, loads
 
 
 class Base:
@@ -13,3 +15,16 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Static method return JSON serialization of list dic.
+
+        Args:
+            list_dictionaries (list): list of dictionaries.
+        """
+        if list_dictionaries is None:
+            return "[]"
+        if list_dictionaries is list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
