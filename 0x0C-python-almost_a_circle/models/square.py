b98224a0ce15ddc2288e0f39aba8c369c16d95b7
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-12-10 08:40:43 +0400

    args&kwargs

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,17 +9,17 @@ class Square(Rectangle):
     """Defines square class inharit from rectangle
 
     Args:
-                    Rectangle:supper class
+                                    Rectangle:supper class
     """
 
     def __init__(self, size, x=0, y=0, id=None):
         """constructor for initialize object of class
 
         Args:
-                        size (int): size of square
-                        x (int): x coordinate of square. Defaults to 0.
-                        y (int): y coordinate of square. Defaults to 0.
-                        id (int): id of square. Defaults to None.
+                                        size (int): size of square
+                                        x (int): x coordinate of square. Defaults to 0.
+                                        y (int): y coordinate of square. Defaults to 0.
+                                        id (int): id of square. Defaults to None.
         """
         super().__init__(size, size, x, y, id)
 
@@ -27,7 +27,7 @@ class Square(Rectangle):
         """Define string module for square.
 
         Returns:
-                        return: return string format for square
+                                        return: return string format for square
         """
         return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x,  self.y, self.width)
 
@@ -41,3 +41,44 @@ class Square(Rectangle):
         """Module for setting size of Square."""
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """Define update the Square module.
+
+        Args:
+                *args (ints): New arrguments values.
+                        - 1 argument represents id attribute
+                        - 2 argument represents size attribute
+                        - 3 argument represents x attribute
+                        - 4 argument represents y attribute
+                **kwargs (dict): New key/value pairs of attributes.
+        """
+
+        if args:
+            for i in range(len(args)):
+                if i == 0:
+                    if type(args[i]) is not int:
+                        raise TypeError("id must be an integer")
+                    self.id = args[i]
+                elif i == 1:
+                    self.width = args[i]
+                elif i == 2:
+                    self.height = args[i]
+                elif i == 3:
+                    self.x = args[i]
+                elif i == 4:
+                    self.y = args[i]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    if type(value) is not int:
+                        raise TypeError("id must be an integer")
+                    self.id = value
+                elif key == "width":
+                    self.width = value
+                elif key == "height":
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
