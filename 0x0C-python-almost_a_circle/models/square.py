b37c3a35fccbb650db8c70eaca2a65cb825eaaf3
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-12-10 08:20:54 +0400

    square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines module for square class"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Defines square class inharit from rectangle
+
+    Args:
+            Rectangle:supper class
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """constructor for initialize object of class
+
+        Args:
+                size (int): size of square
+                x (int): x coordinate of square. Defaults to 0.
+                y (int): y coordinate of square. Defaults to 0.
+                id (int): id of square. Defaults to None.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Define string module for square.
+
+        Returns:
+                return: return string format for square
+        """
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x,  self.y, self.width)
