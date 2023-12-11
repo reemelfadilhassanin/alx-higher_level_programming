1cbf7a991b485ed4575e173f1a1ef69c6e2b6348
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-12-11 20:48:15 +0400

    getter

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -23,7 +23,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Define private attr of rectangle"""
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -37,7 +37,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Define private attr of rectangle"""
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -51,7 +51,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Define private attr of rectangle"""
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -75,7 +75,7 @@ class Rectangle(Base):
         """Define public methof of rectangle
 
         Returns:
-                                                                        Return: return the area
+                                                                                                                                        Return: return the area
         """
         return self.width * self.height
 
