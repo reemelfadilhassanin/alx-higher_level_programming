fe17db007c905d7602f02d3c6ba293afa71738a6
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-12-11 22:00:06 +0400

    dummy

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -70,13 +70,12 @@ class Base:
         Returns:
             return: instance with all attributes already set
         """
-        if dictionary and dictionary != {}:
-            if cls.__name__ == "Rectangle":
-                new = cls(1, 1)
-            else:
-                new = cls(1)
-            new.update(**dictionary)
-            return new
+        if cls.__name__ == 'Rectangle':
+            dummy = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
 
     @classmethod
     def load_from_file(cls):
