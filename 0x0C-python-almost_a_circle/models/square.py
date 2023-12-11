f9e7643353d59abb80cb9834f42d88165914f842
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-12-11 21:45:36 +0400

    square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -41,35 +41,10 @@ class Square(Rectangle):
     def update(self, *args, **kwargs):
         """Define update the Square module
         """
-
         if args:
-            for i in range(len(args)):
-                if i == 0:
-                    if type(args[i]) is not int:
-                        raise TypeError("id must be an integer")
-                    self.id = args[i]
-                elif i == 1:
-                    self.width = args[i]
-                elif i == 2:
-                    self.height = args[i]
-                elif i == 3:
-                    self.x = args[i]
-                elif i == 4:
-                    self.y = args[i]
-        else:
-            for key, value in kwargs.items():
-                if key == "id":
-                    if type(value) is not int:
-                        raise TypeError("id must be an integer")
-                    self.id = value
-                elif key == "width":
-                    self.width = value
-                elif key == "height":
-                    self.height = value
-                elif key == "x":
-                    self.x = value
-                elif key == "y":
-                    self.y = value
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
 
     def to_dictionary(self):
         """Return the dictionary information of a square."""
