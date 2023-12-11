2e4835c232805fb4cb997b89816ba674a98a481e
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-12-11 21:15:25 +0400

    square new

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,17 +9,17 @@ class Square(Rectangle):
     """Defines square class inharit from rectangle
 
     Args:
-                                    Rectangle:supper class
+    Rectangle:supper class
     """
 
     def __init__(self, size, x=0, y=0, id=None):
         """constructor for initialize object of class
 
         Args:
-                                        size (int): size of square
-                                        x (int): x coordinate of square. Defaults to 0.
-                                        y (int): y coordinate of square. Defaults to 0.
-                                        id (int): id of square. Defaults to None.
+        size (int): size of square
+        x (int): x coordinate of square. Defaults to 0.
+        y (int): y coordinate of square. Defaults to 0.
+        id (int): id of square. Defaults to None.
         """
         super().__init__(size, size, x, y, id)
 
@@ -27,7 +27,7 @@ class Square(Rectangle):
         """Define string module for square.
 
         Returns:
-                                        return: return string format for square
+        return: return string format for square
         """
         return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x,  self.y, self.width)
 
@@ -46,12 +46,12 @@ class Square(Rectangle):
         """Define update the Square module.
 
         Args:
-                *args (ints): New arrguments values.
-                        - 1 argument represents id attribute
-                        - 2 argument represents size attribute
-                        - 3 argument represents x attribute
-                        - 4 argument represents y attribute
-                **kwargs (dict): New key/value pairs of attributes.
+        *args (ints): New arrguments values.
+        - 1 argument represents id attribute
+        - 2 argument represents size attribute
+        - 3 argument represents x attribute
+        - 4 argument represents y attribute
+        **kwargs (dict): New key/value pairs of attributes.
         """
 
         if args:
