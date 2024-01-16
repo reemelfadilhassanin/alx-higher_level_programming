f2ed6acec94876e2d9ebda27706fc67e6177a21a
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-01-17 00:07:04 +0400

    utf in db

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,9 @@
+-- Converts hbtn_0c_0 database to UTF8
+
+-- Convert TO hbtn db
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+
+-- Convert table in db
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY COLUMN name varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
