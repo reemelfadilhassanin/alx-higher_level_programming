d8406fe82a89d8670b6d6d3e76f9ac8de69934e6
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-01-18 02:11:39 +0400

    count

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
+SELECT s.title FROM tv_genres g, tv_show_genres t, tv_shows s
+WHERE g.id = t.genre_id
+    AND t.show_id = s.id
+    AND g.name = "Comedy"
+ORDER BY s.title ASC;
+; 
