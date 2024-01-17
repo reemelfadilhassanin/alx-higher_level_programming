9b151c8a3139a4e2c9f1511f80d4ca85860fdd24
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-01-18 02:08:04 +0400

    tv_genres

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,10 @@
 -- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
-SELECT g.name FROM tv_genres g, tv_show_genres t, tv_shows s
-WHERE g.id = t.genre_id
-    AND t.show_id = s.id
-    AND s.title = "Dexter"
-ORDER BY g.name ASC;
-; 
+SELECT g.name
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS s
+       ON g.`id` = s.`genre_id`
+
+       INNER JOIN `tv_shows` AS v
+       ON v.`id` = s.`show_id`
+       WHERE v.`title` = "Dexter"
+ ORDER BY g.`name`
