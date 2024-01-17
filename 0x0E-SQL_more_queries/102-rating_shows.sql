73f714c57f715d2cb7ad2ee8c8975001e508055e
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-01-18 02:27:14 +0400

    task101

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- cript that lists all shows from hbtn_0d_tvshows_rate by their rating.
-SELECT t.title, SUM(tv_show_ratings.rate) AS "rating" FROM t AS t
-LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tt.id
+SELECT t.title, SUM(tv_show_ratings.rate) AS "rating" FROM tv_shows AS t
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = t.id
 GROUP BY t.title
 ORDER BY SUM(tv_show_ratings.rate) DESC
 ;
