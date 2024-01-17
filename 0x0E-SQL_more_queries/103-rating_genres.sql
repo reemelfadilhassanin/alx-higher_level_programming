0c85686d85830a5ef25e2b17be25f0c407b332b1
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-01-18 02:30:23 +0400

    finally

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -4,10 +4,8 @@
 -- Results must be sorted in descending order by their rating
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS "rating" FROM tv_shows
- JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
- JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
- JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+SELECT tv_genres.name, SUM(tv_shows.rating) AS rating_sum
+FROM tv_shows
+JOIN tv_genres ON tv_shows.id = tv_genres.show_id
 GROUP BY tv_genres.name
-ORDER BY SUM(tv_show_ratings.rate) DESC
-;
+ORDER BY rating_sum DESC;
