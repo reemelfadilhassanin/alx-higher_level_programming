4c19aa9b18bbb8318dabe6deb6f2b626fde1da63
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-13 00:46:40 +0400

    js sure

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,14 @@
 #!/usr/bin/node
-const n = parseInt(process.argv[2]);
-if (!isNaN(n)) {
-	for (let i = 0; i < n; i++) {
-		console.log('X'.repeat(n));
+const arg = process.argv[2];
+const size = parseInt(arg);
+if (!isNaN(size) && size > 0) {
+	for (let i = 0; i < size; i++) {
+		let row = '';
+		for (let j = 0; j < size; j++) {
+			row += 'X';
+		}
+		console.log(row);
 	}
 } else {
-	console.log('Missing size');
+	console.log("Missing size");
 }
