9ef54ca0d7288ab0c7d11fa76f8a9c4185baee8d
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-13 02:06:45 +0400

    js biggest

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-function factorial(n) {
-	if (isNaN(n) || n < 2) return (1);
-	return (n * factorial(n - 1));
-}
+
+const factorial = n => isNaN(n) || n < 2 ? 1 : n * factorial(n - 1);
+
 console.log(factorial(process.argv[2]));
