c1b1ac49e3f2bb7fe23853916ee22a7fbce87dd7
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-13 03:09:21 +0400

    fix 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-// print args
+// Prints a message depending of the number of arguments passed
 
-let x;
-const args = process.argv.length;
-if (args < 3) {
-	x = 'No argument';
-} else if (args === 3) {
-	x = 'Argument found';
+if (process.argv.length === 2) {
+	console.log('No argument');
+} else if (process.argv.length === 3) {
+	console.log('Argument found');
 } else {
-	x = 'Arguments found';
+	console.log('Arguments found');
 }
-console.log(x);
