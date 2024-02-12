5dfa23d2a02c67f6ebcdd46667805bcd34e622b5
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-13 03:44:20 +0400

    fix 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const n = Math.floor(Number(process.argv[2]));
-if (!n || isNaN(n)) {
+if (isNaN(n)) {
 	console.log('Missing number of occurrences');
 } else {
-	n = parseInt(n);
 	for (let i = 0; i < n; i++) {
 		console.log('C is fun');
 	}
