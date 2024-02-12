4b19f5d63b025a9b9ffabee39f16ef7b0ddf5e7c
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-13 03:16:32 +0400

    fix 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-const n = parseInt(process.argv[2]);
-if (!isNaN(n)) {
-	console.log('My number: ' + n);
-} else {
+const argv = process.argv[2];
+const num = parseInt(argv);
+if (isNaN(num)) {
 	console.log('Not a number');
+} else {
+	console.log(`My number: ${num}`);
 }
