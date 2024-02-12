9bea1e428719fec2f03cd55da92d96c81277e65d
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-13 01:20:12 +0400

    js args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,10 @@
 let x;
 const argsC = process.argv.length;
 if (argsC === 2) {
-	x = 'No argument';
+    x = 'No argument';
 } else if (argsC === 3) {
-	x = 'Argument found';
+    x = 'Argument found';
 } else {
-	x = 'Arguments found';
+    x = 'Arguments found';
 }
 console.log(x);
