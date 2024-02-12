5d596bc4afeddcba53bc5cd941fa1cbff456abf4
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-12 23:13:07 +0400

    js loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-for (const l of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-	console.log(l);
+const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < lines.length; i++) {
+	console.log(lines[i]);
 }
