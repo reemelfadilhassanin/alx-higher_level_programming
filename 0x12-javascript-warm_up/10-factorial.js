1f68e67aed51d7df52b06149ec6894025f3cf6fd
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-13 01:59:13 +0400

    js factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,20 +1,10 @@
 #!/usr/bin/node
 function factorial(n) {
-	// Base case: factorial of 0 or 1 is 1
-	if (n === 0 || n === 1) {
+	if (isNaN(n) || n < 2) {
 		return 1;
+	} else {
+		return n * factorial(n - 1);
 	}
-	// Recursive case: compute factorial using recursion
-	return n * factorial(n - 1);
 }
 
-// Retrieve the first argument passed to the script using process.argv
-const arg = parseInt(process.argv[2]);
-
-// Check if the argument is NaN
-if (isNaN(arg)) {
-	console.log(1); // Factorial of NaN is 1
-} else {
-	// Compute and print the factorial
-	console.log(factorial(arg));
-}
+console.log(factorial(process.argv[2]));
