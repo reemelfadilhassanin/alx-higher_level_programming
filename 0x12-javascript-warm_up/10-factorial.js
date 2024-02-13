37ae4394e802215a7e181542aed91728c578d78f
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-13 04:14:25 +0400

    fix 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
+
 function factorial(n) {
-	if (isNaN(n) || n < 2) {
+	if ((isNaN(n)) || (n === 1)) {
 		return 1;
 	} else {
 		return n * factorial(n - 1);
 	}
 }
 
-console.log(factorial(process.argv[2]));
+console.log(factorial(parseInt(process.argv[2])));
