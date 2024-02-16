ea3004d0c10a37dfa68d89b911a07d3f0e57859f
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-16 19:04:37 +0400

    list state with filiters

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python3
 """this to  lists all states start with N from hbtn_0e_0_usa
 """
-import sys
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    dbconn = MySQLdb.connect(
-        host="localhost",
-        user=username,
-        passwd=password,
-        db=database,
-        port=3306
-    )
+    mysql_username, mysql_password, database_name = sys.argv[1:]
+    dbconn = MySQLdb.connect(host="localhost", user=mysql_username,
+                             passwd=mysql_password, db=database_name)
     curs = dbconn.cursor()
-    curs.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    find = curs.fetchall()
-    for line in find:
+    query = """
+        SELECT *
+        FROM states
+        WHERE name LIKE BINARY 'N%'
+        ORDER BY states.id
+    """
+
+    curs.execute(query)
+
+    res = curs.fetchall()
+
+    for line in res:
         print(line)
     curs.close()
     dbconn.close()
