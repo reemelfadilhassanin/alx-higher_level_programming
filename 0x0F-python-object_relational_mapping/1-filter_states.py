670bb88e9da4c455bc1b6b03238ca509bb939c22
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-16 18:56:01 +0400

    list state with filiters

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""this to lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa
+"""this to lists all states with a name starting with N (upper N)from hbtn_0e_0_usa
 """
 import sys
 import MySQLdb
