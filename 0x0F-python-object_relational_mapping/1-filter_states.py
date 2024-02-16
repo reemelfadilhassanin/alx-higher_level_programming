32e8c83fb59d89534d9e84f1f81b07f9c805cfed
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-16 18:45:55 +0400

    list state with filiters

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""this to lists all states with a name starting with N (upper N) 
+from the database hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    dbconn = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
+    )
+    curs = dbconn.cursor()
+    curs.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    find = curs.fetchall()
+    for line in find:
+        print(line)
+    curs.close()
+    dbconn.close()
