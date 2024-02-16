9ca99e2d721637878c76238e74001494668cc6fa
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-16 17:15:23 +0400

    first select state

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    dbconn = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
+    )
+    curs = dbconn.cursor()
+    curs.execute("SELECT * FROM states ORDER BY states.id ASC")
+    find = curs.fetchall()
+    for row in find:
+        print(row)
+    curs.close()
+    dbconn.close()
