8ec6355bb14cdf157ffbb6653d8d6d903e0c855a
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-16 19:18:14 +0400

    list state with filiters state name

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Script that displays all values in the states table of hbtn_0e_0_usa
-   where name matches the argument"""
+"""This takes in an argument and displays all values in 
+	the states table of hbtn_0e_0_usa where name matches the argumen"""
 
 import MySQLdb
 import sys
