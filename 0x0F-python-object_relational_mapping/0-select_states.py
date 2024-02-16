ab659227d9b06c159582a6ba6cf932fb0da80965
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-16 18:53:20 +0400

    list state with filiters

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,9 +5,21 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    dbconn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                             passwd=sys.argv[2], db=sys.argv[3])
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
 
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    dbconn = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
+    )
     curs = dbconn.cursor()
     curs.execute("SELECT * FROM states ORDER BY states.id ASC")
     find = curs.fetchall()
