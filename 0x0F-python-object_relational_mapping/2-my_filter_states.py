a6a8f0c5835b67f5785cff1c33a93d1079bde0e8
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-16 19:24:29 +0400

    list state with filiters state name

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
-"""This takes in an argument and displays all values in 
-	the states table of hbtn_0e_0_usa where name matches the argumen"""
-
+"""this to takes in an argument and displays all values in the states table
+	from hbtn_0e_0_usa
+"""
+from sys import argv
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: {} username password database state_name".format(
-            sys.argv[0]))
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
         sys.exit(1)
 
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    state_name = sys.argv[4]
-    dbconn = MySQLdb.connect(host="localhost", port=3306, user=username,
-                             passwd=password, db=database)
-
+    dbconn = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
+    )
     curs = dbconn.cursor()
+    query = """
+        SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC
+    """
+    query = query.format(argv[4])
 
-    curs.execute(
-        "SELECT * FROM states WHERE name = %s ORDER BY id ASC", (state_name,))
+    curs.execute(query)
 
     res = curs.fetchall()
 
