8f51e088c0adabdb57c4e794e21f528738f1762b
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-17 21:06:11 +0400

    state name ocuurenace

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -8,16 +8,17 @@ from model_state import State
 from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(argv[1], argv[2], argv[3]))
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-    session_maker = sessionmaker(bind=engine)
-    session = session_maker()
-
-    for state in session.query(State):
-        if (state.name == argv[4]):
-
-            print(state.id)
-            break
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).\
+        filter(State.name == argv[4]).order_by(State.id).all()
+    if states:
+        print("{}".format(states[0].id))
     else:
         print("Not found")
+    session.close()
