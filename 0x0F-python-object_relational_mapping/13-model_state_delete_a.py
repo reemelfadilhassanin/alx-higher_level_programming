22d76a447fe05a03a2c53d5337d2bc88b0bd1ed4
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-17 21:34:00 +0400

    delet all obj with a

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """This to deletes all State objects with a name containing the letter a from the database."""
+
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
