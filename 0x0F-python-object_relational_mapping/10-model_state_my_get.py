3b93310a1ce45b10af8247af699327cdbec6125f
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-17 21:04:58 +0400

    state name ocuurenace

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -4,32 +4,20 @@ import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from model_state import State
+from sys import argv
 
 if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(argv[1], argv[2], argv[3]))
 
-    if len(sys.argv) != 5:
-        print("Usage: {} username password database_name state_name".format(
-            sys.argv[0]))
-        sys.exit(1)
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
 
-    db_username = sys.argv[1]
-    db_password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
+    for state in session.query(State):
+        if (state.name == argv[4]):
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(db_username, db_password, db_name),
-                           pool_pre_ping=True)
-
-    Session = sessionmaker(bind=engine)
-
-    session = Session()
-
-    state = session.query(State).filter(State.name == state_name).first()
-
-    if state:
-        print(state.id)
+            print(state.id)
+            break
     else:
         print("Not found")
-
-    session.close()
