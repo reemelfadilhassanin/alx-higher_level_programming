b0f7fce91471acb0ddcefa6a45707a40a6958b5e
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-17 23:44:58 +0400

    delet all obj with a

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,19 +1,34 @@
 #!/usr/bin/python3
 """This prints the State object with the name passed as argument from database"""
 
-from sys import argv
+
+import sys
 from sqlalchemy import create_engine
-from model_state import State
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_state import State
+from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(argv[1], argv[2], argv[3]))
-    session_maker = sessionmaker(bind=engine)
-    session = session_maker()
-    for state in session.query(State):
-        if (state.name == argv[4]):
-            print(state.id)
-            break
+    db_username = sys.argv[1]
+    db_password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(db_username, db_password, db_name),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+
+    db_session = Session()
+
+    first_state = db_session.query(State).filter(
+        State.name == state_name).first()
+
+    if first_state:
+        print(first_state.id)
     else:
         print("Not found")
+
+    db_session.close()
