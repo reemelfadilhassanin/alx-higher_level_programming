41df24477a70ed6047ad24ad244f5aeb0a411202
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-17 18:39:44 +0400

    state first

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This to show first State object from hbtn_0e_6_usa."""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    db_username = sys.argv[1]
+    db_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(db_username, db_password, db_name),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+
+    db_session = Session()
+
+    first_state = db_session.query(State).order_by(State.id).first()
+
+    if first_state:
+        print("{}: {}".format(first_state.id, first_state.name))
+    else:
+        print("Nothing")
+
+    db_session.close()
