97cd2f0732f04eacbe9f6379a9801d96eff6b874
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-17 07:41:53 +0400

    done task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,30 +2,18 @@
 """this to takes in an argument and displays all values in the states table
 """
 from sys import argv
-import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    dbconn = MySQLdb.connect(
-        host="localhost",
-        user=username,
-        passwd=password,
-        db=database,
-        port=3306
-    )
+    dbconn = MySQLdb.connect(user=argv[1], port=3306, host="localhost",
+                             passwd=argv[2], db=argv[3])
 
     curs = dbconn.cursor()
     query = """
 SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC"""
     query = query.format(argv[4])
     curs.execute(query)
+
     res = curs.fetchall()
     for row in res:
         print(row)
