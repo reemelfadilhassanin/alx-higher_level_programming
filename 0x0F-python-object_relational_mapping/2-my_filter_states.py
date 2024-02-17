d48cc41736281729d1e5a0c0009041cc4ea169f2
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-17 07:06:01 +0400

    done task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 """this to takes in an argument and displays all values in the states table
-	from hbtn_0e_0_usa
 """
 from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    dbconn = MySQLdb.connect(user=argv[1], port=3306, host="localhost",
-                             passwd=argv[2], db=argv[3])
-
+    dbconn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                             user=argv[1], passwd=argv[2], db=argv[3])
     curs = dbconn.cursor()
-    curs.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
-    id ASC".format(argv[4]))
-
+    query = """
+SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC"""
+    query = query.format(argv[4])
+    curs.execute(q)
     res = curs.fetchall()
     for row in res:
-        if row[1] == argv[4]:
-            print(row)
+        print(row)
     curs.close()
     dbconn.close()
