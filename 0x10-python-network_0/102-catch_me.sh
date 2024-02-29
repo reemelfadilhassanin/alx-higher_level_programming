fb83899cf3e95663977e6acdd0fc837bef1ee72d
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-29 16:17:14 +0400

    catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a POST request to 0.0.0.0:5000/catch_me
-curl -sX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
