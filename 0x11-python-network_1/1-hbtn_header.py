c4497935a3bc805e5746a8d1738d558051c11aaf
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-03-02 01:57:16 +0400

    header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """Python script that takes in a URL, sends a request to the URL and displays the value 
     """
-import urllib.request
-import sys
+from sys import argv
+from urllib.request import Request, urlopen
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+    url = argv[1]
 
-    re = urllib.request.Request(url)
-    with urllib.request.urlopen(re) as response:
-        x_request_id = response.headers.get('X-Request-Id')
-        print(x_request_id)
+    re = Request(url)
+    with urlopen(re) as response:
+        print(dict(response.headers).get("X-Request-Id"))
