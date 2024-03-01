6995318112afd2084be5f848c3d6c148a44fc171
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-03-02 02:38:51 +0400

    json

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- Python script that takes in a letter and sends a 
+ Python script that takes in a letter and sends a
  POST request to http://0.0.0.0:5000/search_user
 """
 
