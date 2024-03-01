360f3dcd271bb2de2b50a2f15d9a5435cdc4b207
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-03-02 02:20:03 +0400

    status

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,7 +8,6 @@ from requests import get
 import sys
 import requests
 
+
 if __name__ == "__main__":
-    url = sys.argv[1]
-    re = requests.get(url)
     print(get(argv[1]).headers.get('X-Request-Id'))
