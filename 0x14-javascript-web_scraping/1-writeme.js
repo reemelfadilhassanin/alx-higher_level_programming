36aecc02005c2e3185ffd7b7fbca5d7bb09810f1
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-03-26 21:14:59 +0400

    status code

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,14 +1,7 @@
 #!/usr/bin/node
+// a script that writes a string to a file.
 
 const fs = require('fs');
-
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-fs.writeFile(filePath, content, 'utf-8', (err) => {
-	if (err) {
-		console.error(err);
-		process.exit(1);
-	}
-	console.log(`Content has been written to ${filePath}`);
+fs.writeFile(process.argv[2], process.argv[3], (error) => {
+	error && console.log(error);
 });
