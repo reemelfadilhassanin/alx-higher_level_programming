3f3320d900eeaa7616f99dd6e36c0afeebb89a52
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-03-26 23:33:41 +0400

    task 1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 // readme from file
 const fs = require('fs');
-
 const filePath = process.argv[2];
-
 fs.readFile(filePath, 'utf-8', (err, data) => {
 	if (err) {
 		console.error(err);
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 // a script that writes a string to a file.
-
 const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], (error) => {
 	error && console.log(error);
