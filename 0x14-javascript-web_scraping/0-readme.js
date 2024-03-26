1b9cb4392875b2fb4e14853957508da8d4c13bf0
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-03-26 21:00:12 +0400

    c is fun

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', (error, data) => {
-	console.log(error || data);
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+	if (err) {
+		console.error(err);
+		return;
+	}
+	console.log(data);
 });
