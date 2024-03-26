8b0be7c4b9f03622d22331091d6ab68d808c05c9
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-03-26 21:03:04 +0400

    c is fun

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
-//  a script that reads and prints the content of a file.
 
 const fs = require('fs');
+const fName = process.argv[2];
 
-const filePath = process.argv[2];
+if (!fName) {
+	console.log('Usage: node script.js <file_path>');
+	process.exit(1);
+}
 
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.readFile(fName, 'utf-8', function (err, data) {
 	if (err) {
-		console.error(err);
-		return;
+		console.log(err);
+	} else {
+		console.log(data);
 	}
-	console.log(data);
 });
