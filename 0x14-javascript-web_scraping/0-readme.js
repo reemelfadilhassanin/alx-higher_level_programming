54186f632e2210946f87ec71b919788a9a09b7fd
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-03-26 23:39:11 +0400

    task 1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
-// readme from file
 const fs = require('fs');
-const filePath = process.argv[2];
-fs.readFile(filePath, 'utf-8', (err, data) => {
-	if (err) {
-		console.error(err);
-		process.exit(1);
-	}
-	console.log(data);
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+	console.log(error || content);
 });
