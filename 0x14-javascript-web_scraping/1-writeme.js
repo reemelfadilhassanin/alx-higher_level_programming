b17fd52b63601ceb81a5c5ea808e114debb7d96b
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-03-26 21:11:26 +0400

    write js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,14 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], (error) => {
-	error && console.log(error);
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+	if (err) {
+		console.error(err);
+		process.exit(1);
+	}
+	console.log(`Content has been written to ${filePath}`);
 });
