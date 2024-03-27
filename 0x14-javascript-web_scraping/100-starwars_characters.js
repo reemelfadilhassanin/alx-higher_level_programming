680a1f832178a3233e96577675ac4a1d7bbfff15
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-03-27 04:34:41 +0400

    simicolone

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,2 +1,38 @@
 #!/usr/bin/node
-const request = require('request'), movieId = process.argv[2], url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`; request(url, (e, r, b) => { if (e) { console.error(e); return } if (200 !== r.statusCode) { console.error(`Error: Status code ${r.statusCode}`); return } JSON.parse(b).characters.forEach(c => request(c, (e, r, b) => { if (e) { console.error(e); return } if (200 !== r.statusCode) { console.error(`Error: Status code ${r.statusCode}`); return } console.log(JSON.parse(b).name) })) });
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(url, (error, response, body) => {
+	if (error) {
+		console.error(error);
+		return;
+	}
+
+	if (response.statusCode !== 200) {
+		console.error(`Error: Status code ${response.statusCode}`);
+		return;
+	}
+
+	const movieData = JSON.parse(body);
+	const charactersUrls = movieData.characters;
+
+	charactersUrls.forEach(characterUrl => {
+		request(characterUrl, (error, response, body) => {
+			if (error) {
+				console.error(error);
+				return;
+			}
+
+			if (response.statusCode !== 200) {
+				console.error(`Error: Status code ${response.statusCode}`);
+				return;
+			}
+
+			const characterData = JSON.parse(body);
+			console.log(characterData.name);
+		});
+	});
+});
