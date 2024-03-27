fc512ffe83c6a3cc25c6ca5aa5b8c0b50f9cdd7b
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-03-27 04:13:24 +0400

    status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-// Write a script that display the status code of a GET request.
-const request = require('request');
-const url = process.argv[2];
-request(url, function (error, response, body) {
-	error && console.log(error);
-	response && console.log('code:', response.statusCode);
-});
+require('request')(process.argv[2], (_, r) => console.log(`code: ${r.statusCode}`))
