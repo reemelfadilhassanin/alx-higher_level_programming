10494bb17f5672e0c5619f89c425615c19339c1a
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-03-27 04:31:20 +0400

    simicolone

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,8 +4,12 @@
 const request = require('request');
 
 request(process.argv[2], (error, response, body) => {
-	!error && console.log(JSON.parse(body).reduce(function (all, curr) {
-		curr.completed && (all[curr.userId] = (all[curr.userId] || 0) + 1);
-		return all;
-	}, {}));
+	if (!error) {
+		console.log(JSON.parse(body).reduce(function (all, curr) {
+			if (curr.completed) {
+				all[curr.userId] = (all[curr.userId] || 0) + 1;
+			}
+			return all;
+		}, {}));
+	}
 });
