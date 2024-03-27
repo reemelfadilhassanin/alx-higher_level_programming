b49dd63454dc88ee113e77f2b74aaaaa01dd0aba
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-03-27 04:27:42 +0400

    simicolone

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-//requat to store
+
+// Require to store
 const fs = require('fs');
 const request = require('request');
+
 request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
