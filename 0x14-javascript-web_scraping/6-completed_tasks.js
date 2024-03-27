f09c2530e219af7d7a7b76ca81136e1aa2df202b
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-03-27 04:29:55 +0400

    simicolone

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-// comlet the task
+
+// Complete the task
 const request = require('request');
+
 request(process.argv[2], (error, response, body) => {
 	!error && console.log(JSON.parse(body).reduce(function (all, curr) {
 		curr.completed && (all[curr.userId] = (all[curr.userId] || 0) + 1);
